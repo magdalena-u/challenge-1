e9eebfd91f2fe7fd2558665aa8f7bc16d45de812
magda-u <magdalena.urzon@gmail.com>
2018-10-10 11:45:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Weekly Challenge 1
+
+Challenge from facebok group Weekly WebDev Challenge
+PSD to html 
+
+## Technologies
+* html
+* CSS
+* javascript
+
+## Source
+fb group Weekly WebDev Challenge: https://www.facebook.com/groups/940002776068923/
+PSD: https://drive.google.com/drive/folders/0Bw2hu70L5Ye_QmVSRzJYNkI5VlU?usp=sharing
+
+## Live
+https://magdalena-u.github.io/challenge-1/
---
+++ script.js
@@ -0,0 +1,14 @@
+var green = document.querySelector(".square1");
+green.addEventListener("click", function () {
+    green.classList.toggle("show")
+})
+
+var green2 = document.querySelector(".square2");
+green2.addEventListener("click", function () {
+    green2.classList.toggle("show")
+})
+
+var green3 = document.querySelector(".square3");
+green3.addEventListener("click", function () {
+    green3.classList.toggle("show")
+})

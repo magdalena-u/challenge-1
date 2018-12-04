316987b19d1c7d6da30e8e6bc9a732fb8e63d14c
magda-u <magdalena.urzon@gmail.com>
2018-12-04 14:59:38 +0100

    improve javascript code

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-var green = document.querySelector(".square1");
-green.addEventListener("click", function () {
-    green.classList.toggle("show")
-})
+const square = document.querySelectorAll(".fa-square");
 
-var green2 = document.querySelector(".square2");
-green2.addEventListener("click", function () {
-    green2.classList.toggle("show")
-})
-
-var green3 = document.querySelector(".square3");
-green3.addEventListener("click", function () {
-    green3.classList.toggle("show")
-})
+square.forEach(item => {
+    item.addEventListener('click', function () {
+        this.classList.toggle('show')
+    }
+    )
+}
+)
